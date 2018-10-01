5b8717284a79a403f31757475c57ce2a047e5d85
Jihad BENABRA <jihad_benabra@carrefour.com>
2018-10-01 14:25:55 +0200

    Version 1.2

--- pytest_yaml/plugin.py
+++ pytest_yaml/plugin.py
@@ -2,8 +2,12 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from yaml import load
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import pytest
+from yaml import load
 
 
 def pytest_addoption(parser):
