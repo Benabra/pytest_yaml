b5978cb7f07f46e6b46edbb97ff386bf493e578b
Jihad BENABRA <jihad_benabra@carrefour.com>
2018-10-05 14:27:58 +0200

    1.2.1

--- tests/sample/test_person.py
+++ tests/sample/test_person.py
@@ -1,5 +1,5 @@
 
-def test_person(yaml_content):
-    print("Test for %s" % yaml_content['test'])
-    for person in yaml_content['persons']:
+def test_person(yaml_output):
+    print("Test for %s" % yaml_output['test'])
+    for person in yaml_output['persons']:
         assert person['age'] == 20, "I am %s" % person['name']
